d04ec260d03944ba3b7da57306b234f1e75029c6
Amit Dash <amit.dash01@sap.com>
2020-05-29 00:05:18 +0530

    Full codebase commit for this grpahql starters repo.

---
+++ data/resolvers.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+import { Friends } from './dbConnectors';
+
+//resolver map
+export const resolvers = {
+    Query: {
+        getFriend: ({ id }) => new Friend(id, friendDatabase[id]),
+        getOneFriend: (root, { id }) => {
+            return new Promise((resolve, object) => {
+                Friends.findById(id, (err, friend) => {
+                    if (err) reject(err)
+                    else resolve(friend)
+                });
+            })
+        },
+        hello: () => "Hi From Graphiql!!",
+    },
+    Mutation: {
+        createFriend: (root, { input }) => {
+            const newFriend = new Friends({
+                firstName: input.firstName,
+                lastName: input.lastName,
+                gender: input.gender,
+                age: input.age,
+                language: input.language,
+                email: input.email,
+                contacts: input.contacts
+            });
+            
+            newFriend.id = newFriend._id;
+
+            return new Promise((resolve, object) => {
+                newFriend.save((err) => {
+                    if(err) reject(err);
+                    else resolve(newFriend)
+                })
+            })
+        },
+        updateFriend: (root, {input}) => {
+            return new Promise((resolve, object) => {
+                Friends.findOneAndUpdate({_id: input.id}, input, {new: true}, (err, friend) => {
+                    if(err) reject(err);
+                    else resolve(friend);
+                })
+            })
+        },
+        deleteFriend: (root, {id}) => {
+            return new Promise((resolve, object) => {
+                Friends.remove({_id: id}, (err) => {
+                    if(err) reject(err);
+                    else resolve("Successfully deleted Friend");
+                })
+            })
+        }
+    },
+
+};
